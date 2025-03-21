1e2b70066fa963c1750d67209db8b743ee29633e
Elisee Augustin <eliseeaugustin@elisees-mbp-2.mynetworksettings.com>
2025-03-21 13:12:35 -0400

    Made changes to User schema and added Activate token for email validation

--- middleware.ts
+++ middleware.ts
@@ -3,5 +3,5 @@ export { default } from 'next-auth/middleware'
 //This will protect any routes defined in here
 //To protect a folder and any subdirectories: '/folder/:path*, can use on app directory if necessary
 export const config = {
-  matcher: ['/dashboard'],
+  matcher: ['/dashboard'], //Example
 }
