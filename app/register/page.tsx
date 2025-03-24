431e9c0b3788a216e8ae9493da737348f43cb0f5
Elisee Augustin <eliseeaugustin@gmail.com>
2025-03-24 18:31:51 -0400

    Added mailgun and updated some constants for it

--- app/register/page.tsx
+++ app/register/page.tsx
@@ -2,11 +2,24 @@ import Link from 'next/link'
 import styles from './register.module.css'
 import { hash } from 'bcrypt'
 import { prisma } from '@/lib/prisma'
+import { randomUUID } from 'crypto'
+import formData from 'form-data' // form-data v4.0.1
+import Mailgun from 'mailgun.js' // mailgun.js v11.1.0
+
+const API_KEY = process.env.MAILGUN_API_KEY || ''
+const MAILGUN_DOMAIN = process.env.MAILGUN_DOMAIN || ''
+const DOMAIN = process.env.DOMAIN || 'localhost:3000'
+const PROTOCOL = process.env.NODE_ENV == 'production' ? 'https' : 'http'
 
 const Register = () => {
+  const mailgun = new Mailgun(formData)
+  const mg = mailgun.client({
+    username: 'api',
+    key: API_KEY,
+  })
+
   const registerUser = async (data: FormData) => {
     'use server'
-
     const password = await hash(data.get('password') as string, 12)
     const user = await prisma.user.create({
       data: {
@@ -15,6 +28,26 @@ const Register = () => {
         password,
       },
     })
+
+    const token = await prisma.activateToken.create({
+      data: {
+        token: `${randomUUID()}${randomUUID()}`.replace(/-/g, ''),
+        userId: user.id,
+      },
+    })
+
+    try {
+      const data = await mg.messages.create(MAILGUN_DOMAIN, {
+        from: `Mailgun Sandbox <postmaster@${MAILGUN_DOMAIN}>`,
+        to: [user.email],
+        subject: `Please activate your account`,
+        text: `Hello ${user.name}, please activate your account by clicking this link: ${PROTOCOL}://${DOMAIN}/activate/${token.token}`, //TODO: Should put link portion(localhost:3000) in an env file
+      })
+
+      console.log('mailgun data: ', data) // logs response data
+    } catch (error) {
+      console.log(error) //logs any error
+    }
   }
 
   return (
