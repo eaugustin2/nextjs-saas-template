f76b10b0fd003c06874464a9c9cf47711a29f93f
Elisee Augustin <eliseeaugustin@elisees-mbp-2.mynetworksettings.com>
2025-03-28 08:24:57 -0400

    Created a client side form for Login page because of signin method from next-auth

---
+++ app/login/form.tsx
@@ -0,0 +1,69 @@
+'use client'
+import { useSearchParams, useRouter } from 'next/navigation'
+import { useState } from 'react'
+import styles from './login.module.css'
+import { signIn } from 'next-auth/react'
+
+export const Form = () => {
+  const router = useRouter()
+  const searchParams = useSearchParams()
+  const callbackUrl = searchParams.get('callbackUrl') || '/dashboard'
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+  const [error, setError] = useState('')
+
+  const loginUser = async (e: React.FormEvent) => {
+    e.preventDefault()
+    try {
+      const res = await signIn('credentials', {
+        redirect: false,
+        email,
+        password,
+        callbackUrl,
+      })
+      console.log('Res', res)
+      if (!res?.error) {
+        router.push(callbackUrl)
+      } else {
+        setError('Invalid email or password')
+      }
+    } catch (err: any) {}
+  }
+
+  return (
+    <form onSubmit={loginUser} className={styles.form}>
+      <div className={styles.formInputContainer}>
+        <span className={styles.formLabel}>Email:</span>
+        <input
+          name="email"
+          type="email"
+          className={styles.formInput}
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          required
+        />
+      </div>
+      <div className={styles.formInputContainer}>
+        <span className={styles.formLabel}>Password:</span>
+        <input
+          name="password"
+          type="password"
+          className={styles.formInput}
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          required
+        />
+      </div>
+      {error && (
+        <div className={styles.errorContainer}>
+          <span className={styles.errorMessage}>{error}</span>
+        </div>
+      )}
+      <div className={styles.formButtonContainer}>
+        <button className={styles.formButton} type="submit">
+          Sign In
+        </button>
+      </div>
+    </form>
+  )
+}
