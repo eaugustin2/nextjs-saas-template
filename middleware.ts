d30e029cac3daeb6888ac23a1bf823ca2f399580
Elisee Augustin <eliseeaugustin@elisees-mbp-2.mynetworksettings.com>
2025-03-28 08:51:53 -0400

    Created an auth route grouping and updated middleware to allow access only to whitelisted routes

--- middleware.ts
+++ middleware.ts
@@ -2,6 +2,13 @@ export { default } from 'next-auth/middleware'
 
 //This will protect any routes defined in here
 //To protect a folder and any subdirectories: '/folder/:path*, can use on app directory if necessary
+// export const config = {
+//   matcher: ['/dashboard'], //Example
+// }
+
+//We are whitelisting what routes not to protect here. Any route not listed below is protected
 export const config = {
-  matcher: ['/dashboard'], //Example
+  matcher: [
+    '/((?!register|api|login|forgot-password|password-reset|activate).*)',
+  ],
 }
