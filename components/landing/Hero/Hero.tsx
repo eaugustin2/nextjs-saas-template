6e375df48bf26825dd24439744ae6b28a5b6635b
Elisee Augustin <eliseeaugustin@gmail.com>
2025-08-19 09:55:13 -0400

    added styling for hero section

--- components/landing/Hero/Hero.tsx
+++ components/landing/Hero/Hero.tsx
@@ -1,8 +1,24 @@
+import CTAButton from '../CTAButton/CTAButton'
+
 const Hero = () => {
   return (
-    <section className="flex flex-1 flex-row items-center justify-center bg-gray-50">
-      <div>Content</div>
-      <div>Media</div>
+    <section className="flex flex-1 flex-row items-center bg-gray-50">
+      <div className="flex w-2/4 flex-col items-center justify-center gap-3">
+        <h1 className="text-4xl font-bold md:text-5xl">Hero Title</h1>
+        <p className="text-lg text-gray-600 md:text-xl">
+          Hero subtitle/description
+        </p>
+        <div className="flex flex-row items-center justify-center gap-2">
+          <CTAButton href="/start" variant="outline" size="lg">
+            Get Started
+          </CTAButton>
+          <span>Or</span>
+          <CTAButton href="/" variant="ghost" size="lg">
+            Demo
+          </CTAButton>
+        </div>
+      </div>
+      <div className="flex w-2/4 items-center justify-center">Media</div>
     </section>
   )
 }
