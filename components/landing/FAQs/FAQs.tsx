8d6f220ce3f0ee0b699f9d2e6c4b362a433a7ee0
Elisee Augustin <eliseeaugustin@gmail.com>
2025-08-22 17:28:13 -0400

    Added header for FAQs

--- components/landing/FAQs/FAQs.tsx
+++ components/landing/FAQs/FAQs.tsx
@@ -7,7 +7,11 @@ import {
 
 const FAQs = () => {
   return (
-    <section className="my-20">
+    <section className="my-20 flex flex-col gap-12">
+      <div className="flex w-full flex-col items-center justify-center">
+        <h2 className="text-3xl font-bold">Frequently Asked Questions</h2>
+        <p className="text-lg text-gray-600">Description for FAQ section</p>
+      </div>
       <Accordion type="single" collapsible className="mx-20 flex flex-col">
         <AccordionItem value="value-1">
           <AccordionTrigger className="w-full cursor-pointer">
