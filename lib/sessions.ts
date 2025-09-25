74bffaf8eaf75cebed3603b6c28d466b6a668343
Elisee Augustin <eliseeaugustin@gmail.com>
2025-09-25 09:47:17 -0400

    updated authOptions import for session.ts

--- lib/sessions.ts
+++ lib/sessions.ts
@@ -1,5 +1,5 @@
 import { getServerSession } from 'next-auth'
-import { authOptions } from '@/app/api/auth/[...nextauth]/route'
+import { authOptions } from './auth'
 
 //helper func to get session data and custom data on server but not have to always put authOptions
 export function getSession() {
